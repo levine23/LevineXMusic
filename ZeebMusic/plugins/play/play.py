e829faef50b9d8f730f9a608a0ff28f98a5f977c
Levine <annacantik461@gmail.com>
2025-08-23 21:06:22 +0900

    Update play.py

--- ZeebMusic/plugins/play/play.py
+++ ZeebMusic/plugins/play/play.py
@@ -56,10 +56,10 @@ async def play_commnd(
     url,
     fplay,
 ):
-if channel:
-    mystic = await message.reply_sticker(_["play_2"])
-else:
-    mystic = await message.reply_sticker(_["play_1"])
+    if channel:
+        mystic = await message.reply_sticker(_["play_2"])
+    else:
+        mystic = await message.reply_sticker(_["play_1"])
     plist_id = None
     slider = None
     plist_type = None
