0da05c243873a77a01d0fd2fc20e6ecbbb01aa78
Levine <annacantik461@gmail.com>
2025-08-23 18:27:02 +0900

    Update Telegram.py

--- ZeebMusic/platforms/Telegram.py
+++ ZeebMusic/platforms/Telegram.py
@@ -120,14 +120,14 @@ class TeleAPI:
                     completed_size = convert_bytes(current)
                     speed = convert_bytes(speed)
                     text = f"""
-**{app.mention} ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ**
+<blockquote expandable>**{app.mention} ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇᴅɪᴀ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ**
 
 **ᴛᴏᴛᴀʟ ғɪʟᴇ sɪᴢᴇ:** {total_size}
 **ᴄᴏᴍᴘʟᴇᴛᴇᴅ:** {completed_size} 
 **ᴘᴇʀᴄᴇɴᴛᴀɢᴇ:** {percentage[:5]}%
 
 **sᴘᴇᴇᴅ:** {speed}/s
-**ᴇʟᴘᴀsᴇᴅ ᴛɪᴍᴇ:** {eta}"""
+**ᴇʟᴘᴀsᴇᴅ ᴛɪᴍᴇ:** {eta}</blockquote>"""
                     try:
                         await mystic.edit_text(text, reply_markup=upl)
                     except:
