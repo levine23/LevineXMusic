d47431667de803df0069f4e816a5ca571a7a901d
Levine <annacantik461@gmail.com>
2025-08-23 21:43:21 +0900

    Update skip.py

--- ZeebMusic/plugins/admins/skip.py
+++ ZeebMusic/plugins/admins/skip.py
@@ -2,7 +2,8 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 import config
-from config import BANNED_USERS
+from config import BANNED_USERS, CHANNEL_USERNAME
+from ZeebMusic.utils.decorators import require_fsub
 from strings import get_command
 from ZeebMusic import YouTube, app
 from ZeebMusic.core.call import Zb
@@ -18,6 +19,7 @@ SKIP_COMMAND = get_command("SKIP_COMMAND")
 
 
 @app.on_message(filters.command(SKIP_COMMAND) & filters.group & ~BANNED_USERS)
+@require_fsub
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
     if not len(message.command) < 2:
