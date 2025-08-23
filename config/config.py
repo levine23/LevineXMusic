5eb1e65b63a0da8804f3f4dcb386889c931e14d7
Levine <annacantik461@gmail.com>
2025-08-23 21:24:25 +0900

    Update config.py

--- config/config.py
+++ config/config.py
@@ -99,7 +99,10 @@ SUPPORT_CHANNEL = getenv(
 )  
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", "https://t.me/zonamedia69"
-)  
+)
+
+# Fsub Channel
+CHANNEL_USERNAME = getenv("CHANNEL_USERNAME", "storepler")  
 
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
