dd481a4d6dfe3e30933c30035e1f900887e603c0
Levine <annacantik461@gmail.com>
2025-08-23 18:10:25 +0900

    Update autoleave.py

--- ZeebMusic/plugins/misc/autoleave.py
+++ ZeebMusic/plugins/misc/autoleave.py
@@ -62,7 +62,7 @@ async def auto_end():
                 try:
                     await app.send_message(
                         chat_id,
-                        "Bot has left voice chat due to inactivity to avoid overload on servers. No-one was listening to the bot on voice chat.",
+                        "<blockquote expandable>Bot has left voice chat due to inactivity to avoid overload on servers. No-one was listening to the bot on voice chat.</blockquote>",
                     )
                 except:
                     continue
