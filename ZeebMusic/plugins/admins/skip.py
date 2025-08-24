41f021acfafca524c99eb75896af3abee5b09021
Levine <annacantik461@gmail.com>
2025-08-24 16:54:34 +0900

    Update skip.py

--- ZeebMusic/plugins/admins/skip.py
+++ ZeebMusic/plugins/admins/skip.py
@@ -51,7 +51,7 @@ async def skip(cli, message: Message, _, chat_id):
                                         ),
                                         disable_web_page_preview=True,
                                     )
-                                    await Dante.stop_stream(chat_id)
+                                    await Champu.stop_stream(chat_id)
                                 except:
                                     return
                                 break
@@ -76,7 +76,7 @@ async def skip(cli, message: Message, _, chat_id):
                     disable_web_page_preview=True,
                 )
                 try:
-                    return await Dante.stop_stream(chat_id)
+                    return await Champu.stop_stream(chat_id)
                 except:
                     return
         except:
@@ -85,7 +85,7 @@ async def skip(cli, message: Message, _, chat_id):
                     _["admin_10"].format(message.from_user.first_name),
                     disable_web_page_preview=True,
                 )
-                return await Dante.stop_stream(chat_id)
+                return await Champu.stop_stream(chat_id)
             except:
                 return
     queued = check[0]["file"]
@@ -101,11 +101,11 @@ async def skip(cli, message: Message, _, chat_id):
         if n == 0:
             return await message.reply_text(_["admin_11"].format(title))
         try:
-            await Zb.skip_stream(chat_id, link, video=status)
+            await Champu.skip_stream(chat_id, link, video=status)
         except Exception:
             return await message.reply_text(_["call_7"])
         button = telegram_markup(_, chat_id)
-        img = await gen_thumb(videoid)
+        img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -128,11 +128,11 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             return await mystic.edit_text(_["call_7"])
         try:
-            await Ryn.skip_stream(chat_id, file_path, video=status)
+            await Champu.skip_stream(chat_id, file_path, video=status)
         except Exception:
             return await mystic.edit_text(_["call_7"])
         button = stream_markup(_, videoid, chat_id)
-        img = await gen_thumb(videoid)
+        img = await get_thumb(videoid)
         run = await message.reply_photo(
             photo=img,
             caption=_["stream_1"].format(
@@ -148,7 +148,7 @@ async def skip(cli, message: Message, _, chat_id):
         await mystic.delete()
     elif "index_" in queued:
         try:
-            await Ryn.skip_stream(chat_id, videoid, video=status)
+            await Champu.skip_stream(chat_id, videoid, video=status)
         except Exception:
             return await message.reply_text(_["call_7"])
         button = telegram_markup(_, chat_id)
@@ -161,7 +161,7 @@ async def skip(cli, message: Message, _, chat_id):
         db[chat_id][0]["markup"] = "tg"
     else:
         try:
-            await Ryn.skip_stream(chat_id, queued, video=status)
+            await Champu.skip_stream(chat_id, queued, video=status)
         except Exception:
             return await message.reply_text(_["call_7"])
         if videoid == "telegram":
@@ -196,7 +196,7 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["markup"] = "tg"
         else:
             button = stream_markup(_, videoid, chat_id)
-            img = await gen_thumb(videoid)
+            img = await get_thumb(videoid)
             run = await message.reply_photo(
                 photo=img,
                 caption=_["stream_1"].format(
