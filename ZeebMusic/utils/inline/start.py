11e0f968d54de48cc5b6548b18829b081c413c29
Levine <annacantik461@gmail.com>
2025-08-24 17:26:44 +0900

    Update start.py

--- ZeebMusic/utils/inline/start.py
+++ ZeebMusic/utils/inline/start.py
@@ -10,53 +10,58 @@ def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
+                text=_["S_B_9"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
+            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
         ],
     ]
-    if SUPPORT_CHANNEL and SUPPORT_GROUP:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
-            ]
-        )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
-            )
     return buttons
+
 
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text="Support", url=f"{SUPPORT_CHANNEL}"),
-            InlineKeyboardButton(text="Add Me", url=f"https://t.me/{app.username}?startgroup=true"),
-            InlineKeyboardButton(text="Owner", url=f"https://t.me/skyzx69"),
+            InlineKeyboardButton(
+                text=_["S_B_5"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_3"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text="close", callback_data=f"close"),
-        ]
+            InlineKeyboardButton(
+                text=_["S_B_1"], callback_data="settings_back_helper"
+            )
+        ],
     ]
     return buttons
+
 
 def alive_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="Add Me", url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_9"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
         ],
+    ]
+    return buttons
+
+def music_start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="ᴀᴅᴅ ᴍᴇ ɪɴ ɢʀᴏᴜᴘ",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text="ᴀʙᴏᴜᴛ", callback_data="about"),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", callback_data="support"),
+        ],
+        [InlineKeyboardButton(text="ғᴇᴀᴛᴜʀᴇs", callback_data="feature")],
     ]
     return buttons
