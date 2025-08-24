f3af5657a7872d9e49fd2fe1470488f0470c19f5
Levine <annacantik461@gmail.com>
2025-08-24 22:50:52 +0900

    Update fsub.py

--- ZeebMusic/utils/decorators/fsub.py
+++ ZeebMusic/utils/decorators/fsub.py
@@ -3,6 +3,9 @@ from config import CHANNEL_USERNAME
 
 def require_fsub(func):
     async def wrapper(client, message: Message, *args, **kwargs):
+        if not message.from_user:  # pesan dari channel/anon admin
+            return await func(client, message, *args, **kwargs)
+
         user_id = message.from_user.id
         try:
             member = await client.get_chat_member(f"@{CHANNEL_USERNAME}", user_id)
@@ -10,7 +13,7 @@ def require_fsub(func):
                 raise Exception()
         except:
             return await message.reply_photo(
-                photo="https://files.catbox.moe/s2kmx1.jpg",  # ganti dengan URL gambarmu
+                photo="https://files.catbox.moe/s2kmx1.jpg",
                 caption="<blockquote>ᴜɴᴛᴜᴋ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ʙᴏᴛ ɪɴɪ, sɪʟᴀᴋᴀɴ Jᴏɪɴ ᴄʜᴀɴɴᴇʟ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ.</blockquote>",
                 reply_markup=InlineKeyboardMarkup(
                     [[InlineKeyboardButton("Jᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{CHANNEL_USERNAME}")]]
